f7103343aeff6e33819f052cda94c83972cf28d9
pyx0218 <pyx0218@gmail.com>
2014-04-06 20:00:50 -0400

    naive version of wordnet

--- Antonyms/src/AntonymChecker.java
+++ Antonyms/src/AntonymChecker.java
@@ -1,26 +1,25 @@
 import java.io.FileInputStream;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
 import java.util.zip.GZIPInputStream;
+import rita.wordnet.*;
 
 
 public class AntonymChecker
 {
-	private Map<String,Set<String>> dict=new HashMap<String,Set<String>>();
+	private Map<String,HashSet<String>> dict=new HashMap<String,HashSet<String>>();
+	private RiWordnet wordnet;
 	
-	public AntonymChecker()
-	{
-		
-	}
+	private int questionsCorrect  = 0;
+	private int questionsSkipped  = 0;
+	private int questionsAnswered = 0;
+	private int questionsTotal    = 0;
+	private double precision = 0;
+	private double recall = 0;
+	private double f1 = 0;
 	
+	public AntonymChecker() {}
 	
-	public void init(String path) throws Exception
+	public void initAntLexicon(String path) throws Exception
 	{	
 		Scanner s=new Scanner(new GZIPInputStream(new FileInputStream(path)));
 		int lineCount = 0;
@@ -32,7 +31,7 @@ public class AntonymChecker
 
 			String w1=tokens[0];
 			String w2=tokens[1];
-			Set<String> set=dict.get(w1);
+			HashSet<String> set=dict.get(w1);
 			if(set==null)
 			{
 				set=new HashSet<String>();
@@ -50,6 +49,14 @@ public class AntonymChecker
 		System.out.println("Dictionary Built.");
 	}
 	
+	//Initialize WordNet database
+	public void initWordnet(String path) {
+		//Use WordNet-3.1
+		wordnet = new RiWordnet();
+		//Use WordNet-3.0
+		//wordnet.setWordnetHome(path+"dict\\");
+	}
+	
 	public void runTest(String testPath) throws Exception
 	{
 		GREDataSetParser p=new GREDataSetParser();
@@ -57,26 +64,50 @@ public class AntonymChecker
 		for(GREQuestion q:questions)
 		{
 			questionsTotal++;
-			if(answerIt(q))
+			if(answerByWordnet(q))
 			{
 				questionsCorrect++;
 			}
 		}
+		precision = (double)questionsCorrect/questionsAnswered;
+		recall = (double)questionsCorrect/questionsTotal;
+		f1 = 2*(precision*recall)/(precision+recall);
+		
 		System.out.println("questionsTotal=\t"+questionsTotal);
 		System.out.println("questionsCorrect=\t"+questionsCorrect);
 		System.out.println("questionsSkipped=\t"+questionsSkipped);
 		System.out.println("questionsAnswered=\t"+questionsAnswered);
+		System.out.println("precision=\t"+precision);
+		System.out.println("recall=\t"+recall);
+		System.out.println("F1=\t"+f1);
 	}
 	
-	private int questionsCorrect  = 0;
-	private int questionsSkipped  = 0;
-	private int questionsAnswered = 0;
-	private int questionsTotal    = 0;
+	//Use WordNet to find antonyms
+	public boolean answerByWordnet(GREQuestion q) {
+		String qStr = q.getQuestion();
+		String[] ants = wordnet.getAllAntonyms(qStr, RiWordnet.ADJ);
+
+		if(ants != null) {
+			HashSet<String> set = new HashSet<String>(Arrays.asList(ants));	
+			for(String candidate:q.getOptions())
+			{
+				if(set.contains(candidate))
+				{
+					System.out.println(qStr+":"+candidate);
+					questionsAnswered++;
+					return q.checkAnswer(candidate);
+				}
+			}
+		}
+		questionsSkipped ++;
+		return false;
+	}
 	
-	public boolean answerIt(GREQuestion q)
+	//Use AntonymsLexicon-OppCats-Affixes to find antonyms
+	public boolean answerByAntlexicon(GREQuestion q)
 	{
 		String qStr = q.getQuestion();
-		Set<String> set = dict.get(qStr);
+		HashSet<String> set = dict.get(qStr);
 		if(set!=null)
 		{
 			for(String candidate:q.getOptions())
@@ -95,10 +126,12 @@ public class AntonymChecker
 	public static void main(String[] args) throws Exception
 	{
 		String dictPath="resources/AntonymsLexicon-OppCats-Affixes.gz";
+		String wordnetPath = "resources\\WordNet-3.0\\";
 		String testPath="resources/testset.txt";
 		
 		AntonymChecker t=new AntonymChecker();
-		t.init(dictPath);
+		//t.initAntLexicon(dictPath);
+		t.initWordnet(wordnetPath);
 		t.runTest(testPath);
 	}
 
